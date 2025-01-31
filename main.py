85103e260ab3ab6c6092bf9c25593ba5fd8ff794
mariafaria0511 <maria.faria0511@gmail.com>
2025-01-31 20:53:03 -0300

    Add files via upload
    Branching Assignment Completed

---
+++ main.py
@@ -0,0 +1,61 @@
+#First Example:
+Value1= int(input("Please enter the amount of KW hours:\n"))
+
+first_1000_klw = 0.07633
+over_1000_klw = 0.09259
+
+if Value1 <= 1000:
+    solve = ((Value1 * 7.633) / 100)
+    rounded_number = round(solve, 2)
+    print("Amount owed: ${}".format(rounded_number))
+elif Value1 > 1000:
+    solve = ((1000 * 7.633) + (Value1 - 1000) * 9.259) / 100
+    rounded_number = round(solve, 2)
+    print("Amount owed: ${}".format(rounded_number))
+
+#Second Example:
+Value1= int(input("Please enter the amount of KW hours:\n"))
+
+first_1000_klw = 0.07633
+over_1000_klw = 0.09259
+
+if Value1 <= 1000:
+    solve = ((Value1 * 7.633) / 100)
+    rounded_number = round(solve, 2)
+    print("Amount owed: ${}".format(rounded_number))
+elif Value1 > 1000:
+    solve = ((1000 * 7.633) + (Value1 - 1000) * 9.259) / 100
+    rounded_number = round(solve, 2)
+    print("Amount owed: ${}".format(rounded_number))
+
+#Third Example:
+Value1= int(input("Please enter the amount of KW hours:\n"))
+
+first_1000_klw = 0.07633
+over_1000_klw = 0.09259
+
+if Value1 <= 1000:
+    solve = ((Value1 * 7.633) / 100)
+    rounded_number = round(solve, 2)
+    print("Amount owed: ${}".format(rounded_number))
+elif Value1 > 1000:
+    solve = ((1000 * 7.633) + (Value1 - 1000) * 9.259) / 100
+    rounded_number = round(solve, 2)
+    print("Amount owed: ${}".format(rounded_number))
+
+#Forth Example:
+Value1= int(input("Please enter the amount of KW hours:\n"))
+
+first_1000_klw = 0.07633
+over_1000_klw = 0.09259
+
+if Value1 <= 1000:
+    solve = ((Value1 * 7.633) / 100)
+    rounded_number = round(solve, 2)
+    print("Amount owed: ${}".format(rounded_number))
+elif Value1 > 1000:
+    solve = ((1000 * 7.633) + (Value1 - 1000) * 9.259) / 100
+    rounded_number = round(solve, 2)
+    print("Amount owed: ${}".format(rounded_number))
+
+
